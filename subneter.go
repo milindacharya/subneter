24ee6d09d04e681ad0cfcb5e8c8d5bc089730bde
Milind V. Acharya <milind@milindacharya.com>
2018-12-21 22:54:21 +0000

    work in progress 3, given a number got the first and last number

--- subneter.go
+++ subneter.go
@@ -2,6 +2,8 @@ package subneter
 
 import (
 	"net"
+	"strconv"
+	"strings"
 )
 
 // Subneter keeps all the details of subnet
@@ -20,7 +22,66 @@ func CidrToSubneter(s string) (*Subneter, error) {
 	if err != nil {
 		return nil, err
 	}
-	return calcSubneter(sn), nil
+	a := strings.Split(ip.String(), ".")
+	m, _ := sn.Mask.Size()
+
+	if m >= 1 && m <= 8 {
+		i, _ := strconv.ParseUint(a[0], 10, 8)
+		f, l := getFirstLast(uint8(i), uint8(m))
+		f1 := f + 1
+		l1 := l - 1
+		return &Subneter{
+			NetworkID:   net.IPv4(f, 0, 0, 0),
+			FirstHost:   net.IPv4(f1, 0, 0, 0),
+			LastHost:    net.IPv4(l1, 255, 255, 255),
+			BroadcastIP: net.IPv4(l, 255, 255, 255),
+		}, nil
+	} else if m >= 9 && m <= 16 {
+		x, _ := strconv.ParseUint(a[0], 10, 8)
+		i, _ := strconv.ParseUint(a[1], 10, 8)
+		f, l := getFirstLast(uint8(i), uint8(m))
+		f1 := f + 1
+		l1 := l - 1
+		return &Subneter{
+			NetworkID:   net.IPv4(uint8(x), f, 0, 0),
+			FirstHost:   net.IPv4(uint8(x), f1, 0, 0),
+			LastHost:    net.IPv4(uint8(x), l1, 255, 255),
+			BroadcastIP: net.IPv4(uint8(x), l, 255, 255),
+		}, nil
+	} else if m >= 17 && m <= 24 {
+		i, _ := strconv.ParseUint(a[0], 10, 8)
+		f, l := getFirstLast(uint8(i), uint8(m))
+		f1 := f + 1
+		l1 := l - 1
+		return &Subneter{
+			NetworkID:   net.IPv4(f, 0, 0, 0),
+			FirstHost:   net.IPv4(f1, 0, 0, 0),
+			LastHost:    net.IPv4(l1, 255, 255, 255),
+			BroadcastIP: net.IPv4(l, 255, 255, 255),
+		}, nil
+	} else if m >= 25 && m <= 32 {
+		i, _ := strconv.ParseUint(a[0], 10, 8)
+		f, l := getFirstLast(uint8(i), uint8(m))
+		f1 := f + 1
+		l1 := l - 1
+		return &Subneter{
+			NetworkID:   net.IPv4(f, 0, 0, 0),
+			FirstHost:   net.IPv4(f1, 0, 0, 0),
+			LastHost:    net.IPv4(l1, 255, 255, 255),
+			BroadcastIP: net.IPv4(l, 255, 255, 255),
+		}, nil
+	}
+
+	return nil, nil
+}
+
+// given a number from 0-255, its mask, calculate first and last number
+func getFirstLast(i uint8, m uint8) (uint8, uint8) {
+	f := (i >> (8 - m)) << (8 - m)
+	z := uint8(255)
+
+	l := ((z << m) >> m) ^ f
+	return f, l
 }
 
 // given a subnet, get the network id, broadcast address, first and last ip
--- subneter_test.go
+++ subneter_test.go
@@ -2,9 +2,33 @@ package subneter
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestSomething(t *testing.T) {
 	fmt.Println("Hello")
+}
+
+func Test_getFirstLast(t *testing.T) {
+	tests := []struct {
+		name string
+		num  uint8
+		mask uint8
+		f    uint8
+		l    uint8
+	}{
+		{"0", 201, 1, 128, 255},
+		{"1", 201, 2, 128, 255},
+		{"2", 201, 3, 128, 255},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f, l := getFirstLast(tt.num, tt.mask)
+			fmt.Println(f)
+			fmt.Println(l)
+			assert.Equal(t, tt.f, f)
+			assert.Equal(t, tt.l, l)
+		})
+	}
 }
